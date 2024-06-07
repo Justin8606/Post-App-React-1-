433e5480bd38052c6036bb079a05c38764b4401d
Justin8606 <justinshajuk@gmail.com>
2024-06-07 23:23:33 +0530

    Routing completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import AddPost from './components/AddPost';
 import SearchPost from './components/SearchPost';
 import DeletePost from './components/DeletePost';
+import NavBar from './components/NavBar';
+import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddPost/>
-      <SearchPost/>
-      <DeletePost/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddPost/>}/>
+      <Route path='/Search' element={<SearchPost/>}/>
+      <Route path='/Delete' element={<DeletePost/>}/>
+      <Route path='/View' element={<ViewAll/>}/>
+      
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import NavBar from './NavBar'
+
+const ViewAll = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    
+    
+    return (
+        <div>
+            <NavBar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            {data.map(
+                                (value,index)=>{
+                                    return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                                    <div class="card">
+                                        <div class="card-header">
+                                        {value.userId}
+                                        </div>
+                                        <ul class="list-group list-group-flush">
+                                            <li class="list-group-item">{value.id}</li>
+                                            <li class="list-group-item">{value.title}</li>
+                                            <li class="list-group-item">{value.body}</li>
+                                        </ul>
+                                    </div>
+    
+                                </div>
+                                }
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
