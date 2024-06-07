9eefdd3904bcdf43a4e4bac8e51d73fc169caf58
Justin8606 <justinshajuk@gmail.com>
2024-06-07 21:49:51 +0530

    Delete completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddPost from './components/AddPost';
 import SearchPost from './components/SearchPost';
+import DeletePost from './components/DeletePost';
 
 function App() {
   return (
     <div>
       <AddPost/>
       <SearchPost/>
+      <DeletePost/>
     </div>
   );
 }
